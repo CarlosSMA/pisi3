f8cc2dc2e092341d0f25e3d948e2af70878febfb
Carlos Alvarez <40530061+CarlosSMA@users.noreply.github.com>
2026-09-14 20:42:53 -0300

    Instalar o dash ao inves do streamlit - #4
    Instalar o dash ao inves do streamlit

--- utils/csv.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pandas as pd
-
-
-def get_data_frame_from_csv_file(file_name):
-    print(f"Reading file {file_name}...")
-    df = pd.read_csv(f"{file_name}", on_bad_lines='skip')
-    return df
